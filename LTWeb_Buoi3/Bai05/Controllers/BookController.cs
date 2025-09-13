fd9c71a82bf675b74ccc1235b4ae4185b567341a
thinhphan272 <phanthinh654@gmail.com>
2025-09-13 22:19:01 +0700

    commit Lap

--- LTWeb_Buoi3/Bai05/Controllers/BookController.cs
+++ LTWeb_Buoi3/Bai05/Controllers/BookController.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Bai05.Models;
+using System.IO;
 
 namespace Bai05.Controllers
 {
     public class BookController : Controller
     {
         // GET: Book
+
+        public static List<Book> lstBook = new List<Book>();
         public ActionResult Index()
         {
-
+                
             return View();
+        }
+
+        public ActionResult BookList()
+        {
+
+            return View(lstBook);
         }
 
         public ActionResult Login()
@@ -21,14 +31,40 @@ namespace Bai05.Controllers
         }    
 
 
-        public ActionResult YourInfor()
+        public ActionResult YourInfor(FormCollection fc)
         {
+            string username = fc["username"];
+            string password = fc["password"];
+            ViewBag.Username = "UserName: " + username;
+            ViewBag.Password = "Password: " + password;
             return View();
         }    
 
-        public ActionResult BookInfor()
+        public ActionResult BookInfor(Book bookinfor, HttpPostedFileBase ImageUrl)
         {
-            return View();
+            var valueForm = bookinfor;
+            if(ModelState.IsValid) 
+            {
+                string FileName = "";
+                string Dir = "~/Content/Images";
+                if(ImageUrl != null && ImageUrl.ContentLength > 0)
+                {
+                    FileName = Path.GetFileName(ImageUrl.FileName);
+                    string physicalDir = Server.MapPath(Dir);
+                    if(!Directory.Exists(physicalDir)) 
+                    {
+                        Directory.CreateDirectory(physicalDir);
+                    }
+                    string path = Path.Combine(Server.MapPath(Dir), FileName);
+                    ImageUrl.SaveAs(path);
+                    bookinfor.ImageUrl = Dir + "/" + FileName;
+                      
+                }
+                
+                lstBook.Add(bookinfor);
+                return RedirectToAction("BookList");
+            }
+            return View("Index", bookinfor);
         }    
 
 
