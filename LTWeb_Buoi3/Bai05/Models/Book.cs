413a1c682a2b3ba952ec86c05b9121a8117491f5
thinhphan272 <phanthinh654@gmail.com>
2025-09-13 18:30:06 +0700

    commit PC

---
+++ LTWeb_Buoi3/Bai05/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bai05.Models
+{
+    public class Book
+    {
+        public string bookID {  get; set; }
+        public string bookName { get; set; }
+        public double price { get; set; }
+        public string ImageUrl { get; set; }
+
+        
+    }
+}
